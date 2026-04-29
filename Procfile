f3abdbea75b16d85d447e5ce29d612cad70f83e8
Ryogesh20005 <bxryosh@gmail.com>
2026-04-29 18:48:22 +0530

    first

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: gunicorn wsgi:application
+web: gunicorn --bind 0.0.0.0:$PORT --chdir hospital_backend hospital_project.wsgi:application
 release: cd hospital_backend && python manage.py migrate
--- wsgi.py
+++ wsgi.py
@@ -1,20 +1,21 @@
+#!/usr/bin/env python
 """
 WSGI config for hospital_project project.
-This file is at the root level to facilitate deployment on Render.
 """
 
 import os
 import sys
-from pathlib import Path
 
-# Add the hospital_backend directory to the Python path
-BASE_DIR = Path(__file__).resolve().parent
-sys.path.insert(0, str(BASE_DIR / 'hospital_backend'))
+# Add the hospital_backend directory to Python path
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'hospital_backend'))
 
-# Set Django settings module
+# Set the Django settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hospital_project.settings')
 
-# Import the Django WSGI application
-from django.core.wsgi import get_wsgi_application
+# Import and setup Django
+import django
+django.setup()
 
+# Get the WSGI application
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
